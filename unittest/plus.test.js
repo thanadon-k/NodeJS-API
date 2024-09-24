3bb6c5c8713434db28b906d21dd1369c5029f517
Thanadon Khonthiang <thanadonkhonthiang@MacBook-Air-khxng-Thanadon.local>
2024-09-24 20:29:39 +0700

    edit jenkins

--- unittest/plus.test.js
+++ unittest/plus.test.js
@@ -10,9 +10,15 @@ describe('API Tests', () => {
         expect(response.body.plus).toBe(11);
     });
 
+    it('should return an error if the expression of two numbers is not valid', async () => {
+        const response = await request(app).get('/plus/5.5.5/6.6.6');
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('parameters are not valid numbers');
+    });
+
     it('should return an error if parameters are not numbers', async () => {
         const response = await request(app).get('/plus/foo/bar');
         expect(response.status).toBe(400);
-        expect(response.body.error).toBe('parameters are not numbers');
+        expect(response.body.error).toBe('parameters are not valid numbers');
     });
 });
